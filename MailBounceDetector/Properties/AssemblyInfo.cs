e8bca2cda7f9a37eb1e9754c6fc2be44172dde45
Rui Lopes <rgl@ruilopes.com>
2016-01-18 16:54:29 +0000

    Bump version to 1.0.1.

--- MailBounceDetector/Properties/AssemblyInfo.cs
+++ MailBounceDetector/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ruilopes.com")]
 [assembly: AssemblyProduct("MailBounceDetector")]
-[assembly: AssemblyCopyright("Copyright 2015 ruilopes.com")]
+[assembly: AssemblyCopyright("Copyright ruilopes.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
